eea3622f3d693547a0f4ad7c64e4400548d23139
LoLoL200 <animegood756@gmail.com>
2026-02-26 20:51:28 +0100

    1

--- main.go
+++ main.go
@@ -4,19 +4,13 @@ import (
 	"fmt"
 )
 
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum // отправляем сумму в канал
-}
-
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
-	ch := make(chan int)
-	go sum(s[:len(s)/2], ch)
-	go sum(s[len(s)/2:], ch)
-	x, y := <-ch, <-ch
-	fmt.Println(x, y, x+y)
+	msg := make(chan string, 3)
+	msg <- "Hello"
+	msg <- "World"
+	msg <- "Go"
+
+	fmt.Println(<-msg)
+	fmt.Println(<-msg)
+	fmt.Println(<-msg)
 }
