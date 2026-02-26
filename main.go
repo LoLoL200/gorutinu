c22c5e6c565aa166f250ac4a269537e1d397c071
LoLoL200 <animegood756@gmail.com>
2026-02-26 20:27:29 +0100

    wg group

--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func demoLifecycle() {
-	fmt.Println("1.Start")
-	go func() {
-		fmt.Println("4. не виконуэться")
-	}()
-	fmt.Println("2.готуэться")
-	fmt.Println("3. Основний поток")
-	fmt.Println("5. End")
-
-}
 func main() {
+	var wg sync.WaitGroup
+	wg.Add(100)
 
-	demoLifecycle()
-	time.Sleep(time.Second)
+	go func() {
+		defer wg.Done()
+		time.Sleep(2 * time.Second)
+		fmt.Println("Hello from goroutine")
+	}()
+
+	fmt.Println("hello from main()")
+	wg.Wait()
+	fmt.Println("All goroutines finished")
 }
