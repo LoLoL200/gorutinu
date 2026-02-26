2342d949f8f8c73df19fbd39f34bd17ece4e8891
LoLoL200 <animegood756@gmail.com>
2026-02-26 20:44:43 +0100

    sun example

--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 )
 
-func main() {
-	ch := make(chan int, 2)
-	go func() {
-		a := 5
-		ch <- a
-		ch <- 2312
-	}()
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // отправляем сумму в канал
+}
 
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+	ch := make(chan int)
+	go sum(s[:len(s)/2], ch)
+	go sum(s[len(s)/2:], ch)
+	x, y := <-ch, <-ch
+	fmt.Println(x, y, x+y)
 }
