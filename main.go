928130fcb883e54100e5f743bbd67cfaffb39768
LoLoL200 <animegood756@gmail.com>
2026-03-05 20:27:20 +0100

    2

--- main.go
+++ main.go
@@ -3,43 +3,25 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
-type BankAccount struct {
-	balance int
+var (
+	counter int
 	mu      sync.Mutex
-}
-
-func (ba *BankAccount) Withdraw(amount int) bool {
-	ba.mu.Lock()
-	defer ba.mu.Unlock()
-	if ba.balance >= amount {
-		time.Sleep(time.Microsecond)
-		ba.balance -= amount
-		return true
-
-	}
-
-	return false
-}
-func (ba *BankAccount) GetBalance() int {
-
-	return ba.balance
-}
+)
 
 func main() {
-	account := &BankAccount{balance: 1000}
 	var wg sync.WaitGroup
-
-	for i := range 10 {
+	for range 1000 {
 		wg.Add(1)
-		go func(id int) {
+		go func() {
 			defer wg.Done()
-			success := account.Withdraw(200)
-			fmt.Printf("Goroutine %d: attempt to withdraw 200 uah - %t\n", id, success)
-		}(i)
+			mu.Lock()
+			counter++
+			mu.Unlock()
+
+		}()
 	}
 	wg.Wait()
-	fmt.Printf("Final balance:  %d uah\n", account.GetBalance())
+	fmt.Println("Counter:", counter)
 }
