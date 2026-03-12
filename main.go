1e485a79947a2e80cf1e68bc5374e5d65881bc4c
LoLoL200 <animegood756@gmail.com>
2026-03-12 20:33:33 +0100

    deadline&context

--- main.go
+++ main.go
@@ -6,23 +6,27 @@ import (
 	"time"
 )
 
-func main() {
-	ctx, cencel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cencel()
-
-	done := make(chan struct{})
-	go func() {
-		fmt.Println("Почав роботу...")
-		time.Sleep(3 * time.Second)
-		fmt.Println("Почав роботу...")
-		close(done)
-	}()
+func taskWithDeadline(ctx context.Context, taskName string) {
+	deadline, ok := ctx.Deadline()
+	if ok {
+		fmt.Printf("%s: Dead see for %v\n", taskName, deadline.Format("15:04:05"))
+	}
 
 	select {
-	case <-done:
-		fmt.Println("Операция завершилась успишно")
+	case <-time.After(2 * time.Second):
+		fmt.Printf("%s: Task completed\n", taskName)
 	case <-ctx.Done():
-		fmt.Println("Таймаут! Операция перервана", ctx.Err())
-
+		fmt.Printf("%s:Task cancelled due to deadline: %v\n", taskName, ctx.Err())
 	}
+
+}
+
+func main() {
+	deadline := time.Now().Add(1 * time.Second)
+	ctx, cencel := context.WithDeadline(context.Background(), deadline)
+	defer cencel()
+
+	go taskWithDeadline(ctx, "Task1")
+	go taskWithDeadline(ctx, "Task2")
+	time.Sleep(3 * time.Second)
 }
